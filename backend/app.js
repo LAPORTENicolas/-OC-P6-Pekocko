2e9fb51e40df0d5cfe8acd6984f70a3dd7744462
laportenicolas <laportenicolas@protonmail.com>
2021-03-17 12:28:04 +0100

    Ajout routes et controllers + Header

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,14 @@
 const express   = require('express');
+const routes    = require('./routes/stuff');
 const app       = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use('/api/stuff', routes);
 
 module.exports  = app;
---
+++ backend/routes/stuff.js
@@ -0,0 +1,5 @@
+const express   = require('express');
+const ctrl      = require('../controllers/stuff');
+const router    = express.Router();
+
+module.exports  = router;
