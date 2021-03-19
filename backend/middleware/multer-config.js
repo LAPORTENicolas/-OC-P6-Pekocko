29b5902cdf60586ab7ed278dc8d370a56a0c8e2c
laportenicolas <laportenicolas@protonmail.com>
2021-03-19 13:21:32 +0100

    Correction

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,20 +1,20 @@
-const multer       = require('multer');
+const multer = require('multer');
 
-const MINE_TYPES    = {
+const MIME_TYPES = {
     'image/jpg': 'jpg',
     'image/jpeg': 'jpg',
-    'image/png': 'png',
+    'image/png': 'png'
 };
 
-const storage       = multer.diskStorage({
-    destination: (req, res, callback) => {
-        callback(null, 'images')
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, 'images');
     },
-    filename: (req, res, callback) => {
-        const name      = file.originalname.split(' ').join('_');
-        const extension = MINE_TYPES[file.mimeType];
-        callback(null, + Date.now() + '.' + extension);
+    filename: (req, file, callback) => {
+        const name = file.originalname.split(' ').join('_');
+        const extension = MIME_TYPES[file.mimetype];
+        callback(null, name + Date.now() + '.' + extension);
     }
 });
 
-module.exports      = multer({storage: storage}).single('image')
+module.exports = multer({storage: storage}).single('image');
